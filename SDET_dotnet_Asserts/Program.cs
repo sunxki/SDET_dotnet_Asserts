a2665b687fa5f4facdbb08ebac604399b6d92733
Luis Osuna <luis.osuna@unosquare.com>
2020-05-08 03:13:20 -0500

    Add project files.

---
+++ SDET_dotnet_Asserts/Program.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnoSquare_Maintenance
+{
+    class Program
+    {
+        IWebDriver driver;
+        public IWebDriver SetUpDriver()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            return driver;
+        }
+
+        public void Click(IWebElement element)
+        {
+            element.Click();
+        }
+
+        public void SendText(IWebElement element, string value)
+        {
+            element.SendKeys(value);
+        }
+
+        #region Google Locators
+        By GoogleSearchBar = By.XPath("need maintenance");
+        By GoogleSearIcon = By.XPath("need maintenance");
+        By UnoSquareGoogleResult = By.XPath("need maintenance");
+        #endregion
+
+        #region UnoSquare Locators
+        By UnoSquareServicesMenu = By.XPath("need maintenance");
+        By PracticeAreas = By.XPath("need maintenance");
+        By ContactUs = By.XPath("need maintenance");
+        #endregion 
+        static void Main(string[] args)
+        {
+
+            IWebDriver Browser;
+            IWebElement element;
+            Program program = new Program();
+            Browser = program.SetUpDriver();
+            Browser.Url = "https://www.google.com";
+
+            //Wirite the locator for the Google Search Bar
+            element = Browser.FindElement(program.GoogleSearchBar);
+
+            // Write Assert True that element is present [Google Search] button
+
+            //Send the text "Unosquare" to the Text Bar
+            program.SendText(element, "Unosquare");
+
+            // Click on the Search Button
+            element = Browser.FindElement(program.GoogleSearIcon);
+
+            // Write Assert Equal [Unosquare: Digital Transformation Services | Agile Staffing ...] vs [Element.text]
+
+            // Locate the first result corresponding to Unosquare and click on the Link
+            element = Browser.FindElement(program.UnoSquareGoogleResult);
+
+            program.Click(element);
+
+            // Write Assert True that element is present [Our Dna] menu object
+
+            // Write Assert True that element is present [Articles & Events] menu object
+
+            // Write Assert Equal [Digital transformation solutions] vs [Element.text] h2 ojbect
+
+            //Locate the Service Menu and Click the element
+            element = Browser.FindElement(program.UnoSquareServicesMenu);
+
+            program.Click(element);
+
+            //Locate the Practice Areas Menu and Click the Element
+            element = Browser.FindElement(program.PracticeAreas);
+
+            program.Click(element);
+
+            //Locate the Contact Us Menu and Click the Element
+            element = Browser.FindElement(program.ContactUs);
+
+            program.Click(element);
+
+        }
+    }
+}
---
+++ SDET_dotnet_Asserts/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SDET_dotnet_Asserts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SDET_dotnet_Asserts")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("795ec583-7ba0-4c29-b31b-7939eb297235")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
